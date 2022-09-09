bd750ce5d4f418e6c954e8a790dcfe7d7ba0d2d4
Patrick Black <info@patrickblackjr.com>
2022-09-08 23:38:42 -0500

    boilerplate

---
+++ api/apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: 'domicile-api',
+      url: 'http://localhost:3000/graphql',
+    },
+  },
+}
--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo'
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core'
+import { Module } from '@nestjs/common'
+import { GraphQLModule } from '@nestjs/graphql'
+import { join } from 'path'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TasksModule } from './modules/tasks/tasks.module'
 
 @Module({
-  imports: [],
+  imports: [
+    TasksModule,
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      debug: true,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
