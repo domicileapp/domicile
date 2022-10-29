3b7a5eb21884cb00a0d9a56635f361b295be22a0
Patrick Black <info@patrickblackjr.com>
2022-10-28 19:16:13 -0500

    docs(vuepress): add to release-please configuration
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- docs/src/.vuepress/config.js
+++ docs/src/.vuepress/config.js
@@ -4,7 +4,7 @@ module.exports = {
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#title
    */
-  title: 'Vuepress Docs Boilerplate',
+  title: 'Domicile',
   /**
    * Ref：https://v1.vuepress.vuejs.org/config/#description
    */
@@ -27,7 +27,7 @@ module.exports = {
    * ref：https://v1.vuepress.vuejs.org/theme/default-theme-config.html
    */
   themeConfig: {
-    repo: '',
+    repo: 'https://github.com/domicileapp/domicile',
     editLinks: false,
     docsDir: '',
     editLinkText: '',
@@ -40,10 +40,6 @@ module.exports = {
       {
         text: 'Config',
         link: '/config/'
-      },
-      {
-        text: 'VuePress',
-        link: 'https://v1.vuepress.vuejs.org'
       }
     ],
     sidebar: {
--- docs/src/index.md
+++ docs/src/index.md
@@ -1,15 +1,13 @@
 ---
 home: true
-heroImage: https://v1.vuepress.vuejs.org/hero.png
 tagline: An application to help those with executive dysfunction manage household tasks, inventories, and life.
-actionText: Quick Start →
+actionText: Get started →
 actionLink: /guide/
 features:
-- title: Feature 1 Title
-  details: Feature 1 Description
-- title: Feature 2 Title
-  details: Feature 2 Description
-- title: Feature 3 Title
-  details: Feature 3 Description
-footer: Made by Patrick Black with ❤️
+- title: Chore tracking
+  # details: Feature 1 Description
+- title: Inventory management
+  # details: Feature 2 Description
+- title: Mood tracking
+  # details: Feature 3 Description
 ---
