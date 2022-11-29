917d9090fb96270cf89139b5e36ef563d53169e5
Patrick Black <info@patrickblackjr.com>
2022-11-28 22:54:44 -0600

    eod

--- apps/api/src/rooms/rooms.controller.ts
+++ apps/api/src/rooms/rooms.controller.ts
@@ -6,13 +6,19 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common'
 import { RoomsService } from './rooms.service'
 import { CreateRoomDto } from './dto/create-room.dto'
 import { UpdateRoomDto } from './dto/update-room.dto'
 import { CurrentUser } from '@/auth/decorator/current-user.decorator'
 import { User } from '@/users/user.entity'
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger'
+import { JwtAuthGuard } from '@/auth/guards/jwt-auth.guard'
 
+@ApiTags('rooms')
+@ApiBearerAuth()
+@UseGuards(JwtAuthGuard)
 @Controller('rooms')
 export class RoomsController {
   constructor(private readonly roomsService: RoomsService) {}
