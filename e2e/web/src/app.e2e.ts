a062050e53c9ad4bfeea3d17bad643a1cf279e4b
Patrick Black <info@patrickblackjr.com>
2022-12-27 22:38:25 -0600

    style: commitlint config
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- e2e/web/src/app.e2e.ts
+++
@@ -1,8 +0,0 @@
-// https://docs.cypress.io/api/introduction/api.html
-
-describe('frontend-e2e', () => {
-  it('visits the app root url', () => {
-    cy.visit('/')
-    cy.contains('h1', 'Frontend Demo')
-  })
-})
