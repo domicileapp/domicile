0d6571865f181868468288b1dc6da3526fc157da
Patrick Black <info@patrickblackjr.com>
2023-01-07 20:47:09 -0600

    fix: updates
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,20 +1,41 @@
-/**
- * This is not a production server yet!
- * This is only a minimal backend to get started.
- */
-
-import { Logger } from '@nestjs/common'
+import { ValidationPipe } from '@nestjs/common'
 import { NestFactory } from '@nestjs/core'
-
-import { AppModule } from './app/app.module'
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger'
+import { AppModule } from './app.module'
+import fs from 'fs'
+import path from 'path'
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule)
-  const globalPrefix = 'api'
-  app.setGlobalPrefix(globalPrefix)
-  const port = process.env.PORT || 3333
-  await app.listen(port)
-  Logger.log(`🚀 Application is running on: http://localhost:${port}/${globalPrefix}`)
-}
+  const app = await NestFactory.create(AppModule, {
+    logger: ['error', 'warn', 'log', 'debug'],
+  })
+  app.setGlobalPrefix('api/v1')
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      enableDebugMessages: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  )
 
+  app.enableCors({ origin: 'http://localhost:9000' })
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Domicile')
+    .setDescription(
+      'An application to help those with executive dysfunction manage household tasks, inventories, and life.',
+    )
+    .addBearerAuth()
+    .build()
+
+  const document = SwaggerModule.createDocument(app, swaggerConfig)
+  fs.writeFileSync(
+    path.join(__dirname, '@/src/config/schemas/openapi-spec.json'),
+    JSON.stringify(document, null, 4),
+  )
+  SwaggerModule.setup('api', app, document)
+
+  await app.listen(3000)
+}
 bootstrap()
