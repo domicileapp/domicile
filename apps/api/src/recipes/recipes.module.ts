7d80c2591a6d90064bb7d610f5ab108c3ddec3af
Black Patrick - Nashville <info@patrickblackjr.com>
2023-04-14 19:28:46 -0500

    chore: deps

--- apps/api/src/recipes/recipes.module.ts
+++ apps/api/src/recipes/recipes.module.ts
@@ -4,9 +4,13 @@ import { RecipesController } from './recipes.controller'
 import { MikroOrmModule } from '@mikro-orm/nestjs'
 import { Recipe } from './entity/recipe.entity'
 import { UsersModule } from '@/users/users.module'
+import { RecipeCategory } from './entity/category.entity'
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Recipe]), forwardRef(() => UsersModule)],
+  imports: [
+    MikroOrmModule.forFeature([Recipe, RecipeCategory]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [RecipesController],
   providers: [RecipesService],
 })
