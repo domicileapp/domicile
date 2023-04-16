0ad31ea192f5d15dbc6dd5f4c865ad3bf639295e
Black Patrick - Nashville <info@patrickblackjr.com>
2023-04-16 16:31:48 -0500

    feat(web): edit recipes
    Signed-off-by: Black Patrick - Nashville <info@patrickblackjr.com>

--- apps/web/src/modules/recipes/components/RecipeCard.tsx
+++ apps/web/src/modules/recipes/components/RecipeCard.tsx
@@ -13,7 +13,7 @@ export function RecipeCard({ id, title, ingredients }: Partial<IRecipe>) {
           <Typography variant='body2'>{ingredients}</Typography>
         </CardContent>
         <CardActions sx={{ float: 'right' }}>
-          <Button href={`/recipes/${id}`} startIcon={<Edit />} size='small'>
+          <Button href={`/recipes/${id}/edit`} startIcon={<Edit />} size='small'>
             Edit
           </Button>
           <Button href={`/recipes/${id}`} startIcon={<Visibility />} size='small'>
