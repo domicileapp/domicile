054eeb9e4054d3228a892af9ace7b6c822ddd449
Patrick Black <info@patrickblackjr.com>
2023-05-05 15:00:52 -0500

    chore: move folders around
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { MantineProvider } from '@mantine/core'
 import * as Sentry from '@sentry/react'
 import React from 'react'
@@ -32,3 +33,24 @@ root.render(
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
+=======
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import ReactQuery from '@/providers/ReactQuery'
+import Theme from '@/providers/Theme'
+import Routes from '@/routes/Routes'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+
+import './styles/tailwind.css'
+
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
+    <Theme>
+      <ReactQuery>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Routes />
+      </ReactQuery>
+    </Theme>
+  </StrictMode>
+)
+>>>>>>> ed5ca8e1 (chore: move folders around)
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,38 +1,59 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/// <reference types="vitest" />
-/// <reference types="vite/client" />
-
-import react from '@vitejs/plugin-react'
-import path from 'path'
-import { defineConfig } from 'vite'
-import { VitePWA } from 'vite-plugin-pwa'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+import { defineConfig, loadEnv } from 'vite'
+import compression from 'vite-plugin-compression'
+import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      devOptions: {
-        enabled: true,
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, './environment')
+
+  if (mode === 'production') {
+    process.env = { ...process.env, ...env }
+  }
+
+  const htmlEnvPlugin = () => {
+    return {
+      name: 'html-transform',
+      transformIndexHtml(html) {
+        return html.replace(/%(.*?)%/g, function (_, p1) {
+          return env[p1]
+        })
       },
-      injectRegister: 'auto',
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
-      },
-    }),
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.ts'],
-  },
-  resolve: {
-    alias: [
-      {
-        find: '@',
-        replacement: path.resolve(__dirname, 'src'),
-      },
+    }
+  }
+
+  return {
+    build: {
+      manifest: true,
+      target: 'esnext',
+    },
+    css: {
+      devSourcemap: mode !== 'production',
+    },
+    envDir: 'environment',
+    plugins: [
+      compression(),
+      eslint({
+        cache: false,
+        failOnWarning: false,
+        include: ['src'],
+      }),
+      htmlEnvPlugin(),
+      react(),
     ],
-  },
+    resolve: {
+      alias: [{ find: '@', replacement: resolve(__dirname, 'src') }],
+    },
+    server: {
+      cors: true,
+      open: false,
+    },
+    test: {
+      clearMocks: true,
+      environment: 'happy-dom',
+      include: ['./src/**/*.test.{ts,tsx}'],
+      setupFiles: ['./test/setupTests.ts'],
+    },
+  }
 })
