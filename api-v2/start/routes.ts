9d565321543b5b60042d166a59a5634a217cacdf
Patrick Black <info@patrickblackjr.com>
2023-05-06 16:18:02 -0500

    feat(api): add the rest of the base recipe api routes
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- api-v2/start/routes.ts
+++ api-v2/start/routes.ts
@@ -1,23 +1,3 @@
-/*
-|--------------------------------------------------------------------------
-| Routes
-|--------------------------------------------------------------------------
-|
-| This file is dedicated for defining HTTP routes. A single file is enough
-| for majority of projects, however you can define routes in different
-| files and just make sure to import them inside this file. For example
-|
-| Define routes in following two files
-| ├── start/routes/cart.ts
-| ├── start/routes/customer.ts
-|
-| and then import them inside `start/routes.ts` as follows
-|
-| import './routes/cart'
-| import './routes/customer'
-|
-*/
-
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.get('/', async () => {
@@ -28,7 +8,10 @@ Route.group(() => {
   Route.get('/users', 'UsersController.index')
 
   Route.get('recipes', 'RecipesController.index')
+  Route.get('/recipes/:id', 'RecipesController.view')
   Route.post('recipes', 'RecipesController.create')
+  Route.patch('/recipes/:id', 'RecipesController.update')
+  Route.delete('/recipes/:id', 'RecipesController.delete')
 }).middleware('auth')
 
 Route.post('/users', 'UsersController.create')
