e451ac7665425570e13967ad0bf55832d29a18fb
Patrick Black <info@patrickblackjr.com>
2023-05-07 16:10:24 -0500

    init
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

---
+++ web-v3/src/components/layout/Public.tsx
@@ -0,0 +1,23 @@
+import { LoadingPage } from '@/pages'
+import { Suspense } from 'react'
+import { Outlet } from 'react-router-dom'
+
+const LayoutPublic = () => {
+  return (
+    <div className='w-full flex'>
+      <div className='w-2/4 flex flex-col justify-between py-16 px-14 bg-gray-100'>
+        <div>My public layout</div>
+      </div>
+
+      <div className='w-2/4 bg-white py-16 flex justify-center'>
+        <div className='w-2/4'>
+          <Suspense fallback={<LoadingPage />}>
+            <Outlet />
+          </Suspense>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LayoutPublic
---
+++ web-v3/src/services/api/authentication.ts
@@ -0,0 +1,17 @@
+import IApi from './base/api'
+import { ApiCore } from './base/core'
+
+class AuthenticationApi extends ApiCore<unknown> implements Pick<IApi<unknown>, 'post'> {
+  constructor() {
+    super({
+      methods: ['post'],
+      url: 'login',
+    })
+  }
+
+  // Add/extend custom methods here using te apiProvider
+}
+
+const authentication = new AuthenticationApi()
+
+export default authentication
