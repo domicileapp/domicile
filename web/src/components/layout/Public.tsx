5e9e3c78ea0ccfff23c6d8c49b7c10ebf5b84786
Patrick Black <info@patrickblackjr.com>
2023-05-07 16:20:38 -0500

    move
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- web/src/components/layout/Public.tsx
+++ web/src/components/layout/Public.tsx
@@ -4,9 +4,19 @@ import { Outlet } from 'react-router-dom'
 
 const LayoutPublic = () => {
   return (
-    <Suspense fallback={<LoadingPage />}>
-      <Outlet />
-    </Suspense>
+    <div className='w-full flex'>
+      <div className='w-2/4 flex flex-col justify-between py-16 px-14 bg-gray-100'>
+        <div>My public layout</div>
+      </div>
+
+      <div className='w-2/4 bg-white py-16 flex justify-center'>
+        <div className='w-2/4'>
+          <Suspense fallback={<LoadingPage />}>
+            <Outlet />
+          </Suspense>
+        </div>
+      </div>
+    </div>
   )
 }
 
