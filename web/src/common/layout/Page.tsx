4c7fca24fac93ee8f2c36e6e2d36f33b1af3d260
Patrick Black <info@patrickblackjr.com>
2023-05-07 16:20:38 -0500

    move
    Signed-off-by: Patrick Black <info@patrickblackjr.com>

--- web/src/common/layout/Page.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/material'
-import type { PropsWithChildren } from 'react'
-import { Suspense } from 'react'
-
-export interface PageProps {}
-
-export default function PageWithNavigationSidebar({ children }: PropsWithChildren<PageProps>) {
-  return <Suspense>{children}</Suspense>
-}
